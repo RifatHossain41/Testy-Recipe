3c9740a99c3d514f2da28819891c52ef12a13f30
RifatHossain41 <rifathossain0423@gmail.com>
2023-05-04 15:45:38 +0600

    added card item

--- src/Chef/Chef.jsx
+++ src/Chef/Chef.jsx
@@ -2,19 +2,36 @@ import React from "react";
 import Card from 'react-bootstrap/Card';
 import CardGroup from 'react-bootstrap/CardGroup';
 import "./Chef.css";
+import Row from 'react-bootstrap/Row';
+import { Col } from "react-bootstrap";
 
 const Chef = ({ chef }) => {
-  const { name, picture, likes } = chef;
+  const { _id, name, picture, years_of_experience, num_recipes, likes, description } = chef;
   return (
 
-    <div className="col-lg-4">
-      <div className=" ">
-        <img src={picture} alt="" className="object-cover" />
-        <h2>{name}</h2>
-        <h3>{likes}</h3>
-      </div>
+   <div className="">
+     <Row xs={1} md={2} className="g-4">
       
-    </div>
+      <Card>
+        {/* <Card.Img className="w-12" variant="top" src={picture} /> */}
+        <img src={picture} alt="" />
+        <Card.Body>
+
+          <Card.Title>
+            <h3>{name}</h3>
+          </Card.Title>
+          <Card.Text>
+            <h4>Experience: {years_of_experience}</h4>
+            <h5>Recipes: {num_recipes}</h5>
+            <h5>Likes: {likes}</h5>
+           {description}
+          </Card.Text>
+
+        </Card.Body>
+      </Card>
+    
+</Row>
+   </div>
   );
 };
 
